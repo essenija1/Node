cf392729d98a9ae45ef9aa516256fdad9a98bfe8
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-03-13 12:23:05 +0300

    Update index.js

--- lesson1/index.js
+++ lesson1/index.js
@@ -5,13 +5,16 @@
 
 
 let n = 10;
+const colors = require('colors');
 for (let i = 2; i <= n; i++) {
   for (let j = 2; j <= i; j++) {
     if ((i % j == 0) && (j != i)) {
+
       break;
     } else {
-      console.log(colors.rainbow(i));
+      console.log(colors.red(i));
       break;
+
     }
   }
 }
