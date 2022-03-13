5cdc3164e5cea5f9db08991ae212a7bf1c221251
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-03-13 20:11:27 +0300

    Update index.js

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,21 +1,22 @@
-// const colors = require('colors');
-//console.log(process.argv);
-// const [ name, name2 ] = process.argv.slice(2);
-// console.log(colors.red(`hello Node.js to ${ name } and ${ name2 }`));
-
-
-let n = 10;
 const colors = require('colors');
+let n = 10;
+let color = 'green';
 for (let i = 2; i <= n; i++) {
-  for (let j = 2; j <= i; j++) {
-    if ((i % j == 0) && (j != i)) {
-
-      break;
-    } else {
-      console.log(colors.red(i));
-      break;
-
+    for (let j = 2; j <= i; j++) {
+        if ((i % j == 0) && (j != i)) {
+            break;
+        } else {
+            if (color === 'green') {
+                console.log(colors.green(i));
+                color = 'yellow'
+            } else if (color === 'yellow') {
+                console.log(colors.yellow(i));
+                color = 'red'
+            } else {
+                console.log(colors.red(i));
+                color = 'green'
+            }
+            break;
+        }
     }
-  }
 }
-
